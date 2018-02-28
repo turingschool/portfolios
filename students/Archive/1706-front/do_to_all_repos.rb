7e6b0cc29fc38ea10106e909b8421e59406cac70
Tyler McNamara <mcnamara14@gmail.com>
2018-02-28 12:58:10 -0700

    Update files to most recent

--- students/Archive/1706-front/do_to_all_repos.rb
+++
@@ -1,7 +0,0 @@
-# This script will automate doing a thing to all repos - copy this file into the directory you need to modify, and replace any terminal commands you need to execute inside the each loop.
-
-puts "RUNNING"
-files = Dir.glob('./*')
-files.each do |n|
-  `cp template.markdown #{n}/20170803-blake-griffin-m1.md`
-end
